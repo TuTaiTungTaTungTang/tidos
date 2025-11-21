81f16d397ec67fc214f986058b53371020988c8d
TuTaiTungTaTungTang <taib2105559@student.ctu.edu.vn>
2025-11-22 03:38:14 +0700

    test

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
       clicked.current.parent.getWorldQuaternion(q)
     } else {
       // Default center assumes enlarged portrait height
-      p.set(0, 0.5, 11.5)
+      p.set(0, 0.5, 12)
       q.identity()
     }
   })
